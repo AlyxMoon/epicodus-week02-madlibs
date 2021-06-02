98116a35ac2149d1f02f2da9353e8584bcbe6a51
AlyxMoon <allisterkmoon@gmail.com>
2021-06-02 11:00:56 -0700

    set up the buggy site to follow the examples
    Co-authored-by: Shoshana Newman <sho.newman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $("#formOne").submit(function() {
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
-    const adjectiveInput = $("input#adjective").val();
 
-    $(".person1").text(person1Input);
+    $("person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
-    $(".adjective").text(adjectiveInput);
 
-    $("#story").show();
-
-    event.preventDefault();
+    $("#story").sho();
   });
 });
