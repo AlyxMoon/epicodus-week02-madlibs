c5b4b17798c098da72144265d76d136de50a876e
AlyxMoon <allisterkmoon@gmail.com>
2021-06-02 11:21:21 -0700

    correct the form submission refreshing page
    Co-authored-by: Shoshana Newman <sho.newman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+  $("#formOne").submit(function(event) {
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
@@ -15,5 +15,6 @@ $(document).ready(function() {
     $(".noun").text(nounInput);
 
     $("#story").show();
+    event.preventDefault();
   });
 });
